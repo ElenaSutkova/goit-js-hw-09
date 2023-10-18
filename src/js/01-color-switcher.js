a499f716170b5cbfc13fd3a72089177d4d99f76f
ElenaSutkova <134842923+ElenaSutkova@users.noreply.github.com>
2023-10-18 21:48:03 +0300

    js3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]')
+const body = document.querySelector('body')
+
+let timer = null;
+
+btnStart.addEventListener('click', startBtn);
+btnStop.addEventListener('click', stopBtn);
+
+function startBtn(evt) {
+    evt.target.disabled = true;
+    timer = setInterval(() => {
+         body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function stopBtn() {
+    btnStart.disabled = false;
+    clearInterval(timer);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
