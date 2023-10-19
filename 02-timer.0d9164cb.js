a54b35daf7568d5cc32dcaa34bbccff4389d7dc0
ElenaSutkova <134842923+ElenaSutkova@users.noreply.github.com>
2023-10-19 12:41:50 +0000

    Deploying to gh-pages from @ ElenaSutkova/goit-js-hw-09@5eee7ba46690a2e86eb32073d33e22ebe0eb1591 🚀

--- 02-timer.0d9164cb.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button[data-start]"),document.querySelector(".value[data-days]"),document.querySelector(".value[data-hours]"),document.querySelector(".value[data-minutes]"),document.querySelector(".value[data-seconds]");
-//# sourceMappingURL=02-timer.0d9164cb.js.map
--- 02-timer.a91906a6.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button[data-start]"),document.querySelector(".value[data-days]"),document.querySelector(".value[data-hours]"),document.querySelector(".value[data-minutes]"),document.querySelector(".value[data-seconds]");
-//# sourceMappingURL=02-timer.a91906a6.js.map
