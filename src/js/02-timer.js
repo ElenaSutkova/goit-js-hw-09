235d11ba7da5a79eac58a8f0cd12eeb687e308c8
ElenaSutkova <134842923+ElenaSutkova@users.noreply.github.com>
2023-10-19 12:59:23 +0300

    js5

---
+++ src/js/02-timer.js
@@ -0,0 +1,8 @@
+const selectors = {
+    button: document.querySelector('button[data-start]'),
+
+    day: document.querySelector('.value[data-days]'),
+    hour: document.querySelector('.value[data-hours]'),
+    minute: document.querySelector('.value[data-minutes]'),
+    second: document.querySelector('.value[data-seconds]')
+}
